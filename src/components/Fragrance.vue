f7fc191a5b0836dc92edcb684ff4a582f1e17b19
Boratechlife <boratechlife@gmail.com>
2023-01-21 20:48:20 +0300

    fix carousel in Fragrance

--- src/components/Fragrance.vue
+++ src/components/Fragrance.vue
@@ -10,7 +10,19 @@ const options = ref({
   type: 'loop',
   perPage: 3,
   gap: 30,
+  perMove: 1,
   padding: 30,
+  classes: {
+    // Add classes for arrows.
+    arrows: 'splide__arrows fragrance-arrows ',
+    arrow: 'splide__arrow custom-arrow',
+    prev: 'splide__arrow--prev your-class-prev',
+    next: 'splide__arrow--next your-class-next',
+
+    // Add classes for pagination.
+    pagination: 'splide__pagination your-class-pagination ', // container
+    page: 'splide__pagination__page bg-red-500 pb-20', // each button
+  },
 });
 </script>
 <template>
@@ -92,3 +104,41 @@ const options = ref({
     </Splide>
   </section>
 </template>
+
+<style>
+.fragrance-arrows {
+  position: absolute;
+  top: 100%;
+  left: 45%;
+  transform: translateX(-50%);
+}
+.fragrance-arrows .splide__arrow {
+  top: 110%;
+  transform: translateY(50%);
+}
+
+.fragrance-arrows .splide__arrow--next {
+  right: 0;
+  left: 4rem;
+}
+.fragrance-arrows .splide__arrow--next::before,
+.fragrance-arrows .splide__arrow--prev::before {
+  /* content: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxZW0iIGhlaWdodD0iMWVtIiB2aWV3Qm94PSIwIDAgNDggNDgiPjxnIGZpbGw9Im5vbmUiIHN0cm9rZT0iIzAwMCIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBzdHJva2Utd2lkdGg9IjQiPjxwYXRoIGQ9Ik01Ljc5ODg5IDI0SDQxLjc5ODkiLz48cGF0aCBkPSJNMTcuNzk4OCAzNkw1Ljc5ODgzIDI0TDE3Ljc5ODggMTIiLz48L2c+PC9zdmc+ '); */
+  content: url('data:image/svg+xml; utf8, <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M22 12L2 12M22 12L14 20M22 12L14 4" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>');
+  width: 100%;
+  height: 100%;
+  background-color: #000;
+  opacity: 100;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  z-index: 99999 !important;
+  font-size: x-large;
+  position: absolute;
+}
+.fragrance-arrows .splide__arrow--prev::before {
+  /* content: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxZW0iIGhlaWdodD0iMWVtIiB2aWV3Qm94PSIwIDAgNDggNDgiPjxnIGZpbGw9Im5vbmUiIHN0cm9rZT0iIzAwMCIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBzdHJva2Utd2lkdGg9IjQiPjxwYXRoIGQ9Ik01Ljc5ODg5IDI0SDQxLjc5ODkiLz48cGF0aCBkPSJNMTcuNzk4OCAzNkw1Ljc5ODgzIDI0TDE3Ljc5ODggMTIiLz48L2c+PC9zdmc+ '); */
+  transform: rotate(-180deg);
+}
+</style>
